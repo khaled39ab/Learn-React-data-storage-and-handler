ec41c6bcfa207fcc9be4d4f37bdbb0e063c4ddcb
khaled39ab <khaled39ab@gmail.com>
2022-05-18 22:41:12 +0600

    onclick with parameter

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {product, price, name} = props.cosmetic;
+    const {product, price, name, id} = props.cosmetic;
+
+    const addToCart = (id)=>{
+        console.log("item added", id);
+    }
     return (
-        <div className='compo'>
-            <h4>Product name: {product}</h4>
-            <h4>Buy this: {name}</h4>
-            <h5>Only for: ${price}</h5>
+        <div className='product-area'>
+            <div className='product'>
+                <h4>Product name: {product}</h4>
+                <h4>Buy this: {name}</h4>
+                <h5>Only for: ${price}</h5>
+                <button onClick={()=>addToCart(id)}>Add to cart</button>
+            </div>
         </div>
     );
 };
