0edef875c4a60be3bf8fa773f14a24582bf4438a
khaled39ab <khaled39ab@gmail.com>
2022-05-18 21:58:09 +0600

    problem problem & problem

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Cosmetic = (props) => {
-    // const {product, price, name} = props.cosmetic;
-    console.log(props.cosmetic);
+    const {product, price, name} = props.cosmetic;
+    console.log(props);
     return (
         <div>
-            {/* <h4>Product name: {product}</h4>
+            <h4>Product name: {product}</h4>
             <h4>Buy this: {name}</h4>
-            <h5>Only for: ${price}</h5> */}
+            <h5>Only for: ${price}</h5>
         </div>
     );
 };
